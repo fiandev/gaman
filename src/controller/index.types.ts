b605c419d998619fd9d97abbba7c1fd22c914069
Angga7Togk <kiplihode123321@gmail.com>
2026-03-21 23:12:13 +0700

    refactor: cleanup legacy code and move to src/compose

--- src/controller/index.types.ts
+++
@@ -1,7 +0,0 @@
-import type { ContextHTTP, RequestHandler } from '../context/index.types';
-import type { AppTransportation } from '../index.types';
-
-export type ControllerFactory<
-	T = AppTransportation,
-	Args extends any[] = any[],
-> = (...args: Args) => Record<string, RequestHandler<T>>;
--- src/exception/index.types.ts
+++
@@ -1,1 +0,0 @@
-export type ExceptionHandler = (error: Error) => any;
