a7c9e2f9b715402fb423dcc05ae0807f7d9d4a1a
Angga7Togk <kiplihode123321@gmail.com>
2026-03-22 07:08:19 +0700

    feat: add ctx on ExceptionHandler

--- src/compose/exception/index.ts
+++ src/compose/exception/index.ts
@@ -1,10 +1,9 @@
-import { IS_EXCEPTION_HANDLER } from "../../contants";
+import { IS_EXCEPTION_HANDLER } from '../../contants';
+import type { Context } from '../../types';
 
-export type ExceptionHandler = (error: Error) => any;
+export type ExceptionHandler = (error: any, ctx: Context) => any | Promise<any>;
 
-export function composeException(
-	cb: ExceptionHandler,
-): ExceptionHandler {
+export function composeException(cb: ExceptionHandler): ExceptionHandler {
 	Object.defineProperty(cb, IS_EXCEPTION_HANDLER, {
 		value: true,
 		writable: false,
