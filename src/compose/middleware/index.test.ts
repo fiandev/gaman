534cdb13b5bb9a8305f2c304129bb4054282132c
Angga7Togk <kiplihode123321@gmail.com>
2026-03-25 16:59:57 +0700

    fix(test): middleware/index.test.ts

--- src/compose/middleware/index.test.ts
+++ src/compose/middleware/index.test.ts
@@ -1,43 +1,30 @@
 import { describe, expect, it } from 'bun:test';
-import { composeException, composeMiddleware } from '..';
+import { composeMiddleware } from '..';
 import {
-	IS_EXCEPTION_HANDLER,
-	IS_MIDDLEWARE,
 	IS_MIDDLEWARE_HANDLER,
 } from '../../contants';
-import { Priority } from '../../enums';
 import { Responder } from '../../responder';
 
-const compose = composeMiddleware(
+const middleware = composeMiddleware(
 	(ctx, next) => {
 		return next();
 	},
-	{
-		priority: Priority.HIGH,
-	},
 );
-const middleware = compose();
 
 describe('composeMiddleware', () => {
-	it('is Middleware', () => {
-		// @ts-ignore
-		expect(middleware[IS_MIDDLEWARE]).toBeTrue();
-	});
+
 	it('is Middleware Handler', () => {
 		// @ts-ignore
-		expect(middleware.handler[IS_MIDDLEWARE_HANDLER]).toBeTrue();
+		expect(middleware[IS_MIDDLEWARE_HANDLER]).toBeTrue();
 	});
 
-	it('priority middleware is same', () => {
-		expect(middleware.config.priority).toEqual(Priority.HIGH);
-	});
 
 	it('handler is function in middleware', () => {
-		expect(middleware.handler).toBeFunction();
+		expect(middleware).toBeFunction();
 	});
 
 	it('result is next in middleware', async () => {
 		const res = Responder.ok();
-		expect(await middleware.handler({} as any, () => res)).toEqual(res);
+		expect(await middleware({} as any, () => res)).toEqual(res);
 	});
 });
