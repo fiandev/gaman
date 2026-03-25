53dc6547b8a5ab0fe3c1a39199f0261cde4084ae
Angga7Togk <kiplihode123321@gmail.com>
2026-03-25 23:54:25 +0700

    feat(bench): update benchmark

--- benchmark/gaman.ts
+++ benchmark/gaman.ts
@@ -1,21 +1,20 @@
 import { defineBootstrap } from '../src/index';
 import { composeRouter } from '../src/compose';
-import { Responder as Res } from '../src/responder';
 
 const routes = composeRouter((app) => {
     app.get('/json', (ctx) => {
-        return Res.json({ message: 'Hello World' });
+        return { message: 'Hello World' }
     });
 
     app.post('/form', async (ctx) => {
         const field1 = await ctx.input('field1'); 
-        return Res.json({ field1 });
+        return { field1 };
     });
 
     app.post('/file', async (ctx) => {
         const file = await ctx.file('file');
         const buffer = await file?.arrayBuffer();
-        return Res.json({ filename: file?.filename || 'unknown', size: file?.size || 0, bufferSize: buffer?.byteLength });
+        return { filename: file?.filename || 'unknown', size: file?.size || 0, bufferSize: buffer?.byteLength };
     });
 });
 
